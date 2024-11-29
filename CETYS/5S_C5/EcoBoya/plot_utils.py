0a43f1c0e5dcb44c86c9bcf3767b1561df5a85db
ivanovich99 <142748652+ivanovich99@users.noreply.github.com>
2024-11-28 18:49:00 -0800

    Update plot_utils.py

--- CETYS/5S_C5/EcoBoya/plot_utils.py
+++ CETYS/5S_C5/EcoBoya/plot_utils.py
@@ -25,9 +25,15 @@ class PlotManager:
         self.ax.tick_params(axis='y', labelsize=20)
 
     def actualizar(self, value):
-        self.data_buffer.append(value)
-        self.tiempo.append(len(self.tiempo))
-        self.ax.clear()
-        self.ax.plot(self.tiempo, self.data_buffer, label="Muestras", color='blue')
-        self._configurar_grafica()
-        self.canvas.draw()
+        # Validar que el valor sea un número antes de añadirlo
+        if isinstance(value, list) and len(value) == 1:
+            value = value[0]  # Desempaquetar si es una lista con un solo elemento
+        if isinstance(value, (int, float)):
+            self.data_buffer.append(value)
+            self.tiempo.append(len(self.tiempo))
+            self.ax.clear()
+            self.ax.plot(self.tiempo, self.data_buffer, label="Muestras", color='blue')
+            self._configurar_grafica()
+            self.canvas.draw()
+        else:
+            print(f"Error: Valor inválido recibido para graficar: {value}")
